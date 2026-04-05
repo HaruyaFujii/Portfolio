4bbe89fd3f2c6dcf3c3b66fdfda2b4b6d882470f
HaruyaFujii <sq22612e@st.omu.ac.jp>
2026-04-05 19:47:03 +0900

    fix: linterエラーを修正

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from 'next/link';
 import React, { useState } from 'react';
-import { Menu, X } from 'lucide-react';
+import { Menu } from 'lucide-react';
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import { Button } from '@/components/ui/button';
 
